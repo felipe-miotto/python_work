6cacee862a93611d85d354ce82a0f274105287a4
Felipe Miotto <160151821@aluno.unb.br>
2022-05-30 17:43:34 -0300

    dimmensions and buffet

---
+++ chapter_4/exercises/13_buffet.py
@@ -0,0 +1,9 @@
+menu = ('pasta', 'eggs', 'steak', 'lasagna', 'brie')
+print("The items on the menu are:")
+for items in menu:
+    print(f"- {items.title()}")
+
+menu = ('pasta', 'shrimp', 'steak', 'lasagna', 'bruschetta')
+print("\nThe items on the revised menu are:")
+for items in menu:
+    print(f"- {items.title()}")
