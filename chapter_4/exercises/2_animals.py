bf38f1c035fe6994e2864dc1e36783b82e3e871d
Felipe Miotto <160151821@aluno.unb.br>
2022-05-30 16:16:29 -0300

    animals.py

---
+++ chapter_4/exercises/2_animals.py
@@ -0,0 +1,5 @@
+animals = ['tiger', 'lion', 'monkey']
+for animal in animals:
+    print(f"A {animal} would make a badass pet, if I had all the money in the world!")
+
+print("\nBut of course, all of these animals are difficult to have at home...")
