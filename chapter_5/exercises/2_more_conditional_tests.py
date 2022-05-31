89a3c52ebb7c4e0ac525436e0f2caa30fa44c45e
Felipe Miotto <160151821@aluno.unb.br>
2022-05-31 07:46:08 -0300

    lots of boring tests

---
+++ chapter_5/exercises/2_more_conditional_tests.py
@@ -0,0 +1,35 @@
+one = 'first'
+print("Is the string == 'first'? I predict True.")
+print(one == 'first')
+print("Is the string != 'second'? I predict True.")
+print(one != 'second')
+
+coffee = 'Caramelo e Chocolate'
+print("\nIs the string 'caramelo e chocolate' == coffee.lower()?")
+print('caramelo e chocolate' == coffee.lower())
+
+number = 13
+print("\nIs the number == 13?")
+print(number == 13)
+print("Is the number != 23?")
+print(number != 23)
+print("Is the number <= 10?")
+print(number <= 10)
+print("Is the number >= 50?")
+print(number >= 50)
+print("Is the number > 10?")
+print(number > 10)
+print("Is the number < 100?")
+print(number < 100)
+
+print("\nIs the number > 10 and < 100?")
+print(number > 10 and number < 100)
+print("Is the number < 100 or %3 == 0?")
+print(number < 100 and number % 3 == 0)
+
+lista = [23, 43, 13]
+print("\nIs 13 on the list?")
+print(13 in lista)
+
+print("\nIs 50 not on the list?")
+print(50 not in lista)
