de62155b12c33400d03b6deb57e4121250561a0e
Felipe Miotto <160151821@aluno.unb.br>
2022-05-31 07:32:38 -0300

    conditional examples

---
+++ chapter_5/exercises/1_conditional_tests.py
@@ -0,0 +1,13 @@
+teste = 23
+
+print("Is the number == 23? I predict True")
+print(teste == 23)
+
+print("Is the number >= 23? I predict True")
+print(teste <= 70)
+
+print("Is the number != 23? I predict True")
+print(teste != 13)
+
+print("Is the number <= 23? I predict True")
+print(teste >= 24)
